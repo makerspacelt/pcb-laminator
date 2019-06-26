7f05005437617461ef0364f264184c1715fae8f9
Aivaras Spaicys <aivaras.spaicys@nfq.lt>
2019-06-26 12:10:09 +0300

    whoopsy

--- laminator/laminator.ino
+++ laminator/laminator.ino
@@ -1,8 +1,20 @@
+// resistance at 25 degrees C
+#define THERMISTORNOMINAL 109000      
+// temp. for nominal resistance (almost always 25 C)
+#define TEMPERATURENOMINAL 25   
+// how many samples to take and average, more takes longer
+// but is more 'smooth'
+#define NUMSAMPLES 5
+// The beta coefficient of the thermistor (usually 3000-4000)
+#define BCOEFFICIENT 3950
+// the value of the 'other' resistor
+#define SERIESRESISTOR 100000   
+
 const int RELAYS_COUNT = 4;
-const int SUPER_MAX_TEMPERATURE = 10000;
-const int SUPER_MIN_TEMPERATURE = -10000;
-const int WORK_MAX_TEMPERATURE = 72;
-const int WORK_MIN_TEMPERATURE = 70;
+const int SUPER_MAX_TEMPERATURE = 210;
+const int SUPER_MIN_TEMPERATURE = 10;
+const int WORK_MAX_TEMPERATURE = 187;
+const int WORK_MIN_TEMPERATURE = 184;
 const int NTC_MAP_TEMPERATURE_DELTA = 5;
 const int STATUS_NOT_MAPPED = 0;
 const int STATUS_MAP_RELAYS = 5;
@@ -12,10 +24,12 @@ const int STATUS_DEBUG = 9999;
 int relays[RELAYS_COUNT] = {2, 3, 4, 5};
 int ntc[RELAYS_COUNT] = {A0, A1, A2, A3};
 int ntc_map[RELAYS_COUNT]; // {A3, A2, A0, A1}
-int ntc_temperature_cache[RELAYS_COUNT];
+float ntc_temperature_cache[RELAYS_COUNT];
 int laminator_status;
 int print_delay = 0;
 int print_interval = 2048;
+
+uint16_t samples[NUMSAMPLES];
 
 void switchOffRelay(int relay) {
   digitalWrite(relays[relay], HIGH);
@@ -31,11 +45,35 @@ void switchOffAllRelays() {
   }
 }
 
-int getNtcTemperature (int ntc) {
-  int bits = analogRead(ntc);
-  int temperature = -0.16 * bits + 205;
-  return temperature;
-//  return bits;
+float getNtcTemperature (int ntc) {
+  uint8_t i = 0;
+  float average = 0;
+ 
+  // take N samples in a row, with a slight delay
+  for (i=0; i< NUMSAMPLES; i++) {
+   samples[i] = analogRead(ntc);
+   delay(10);
+  }
+ 
+  // average all the samples out
+  average = 0;
+  for (i=0; i< NUMSAMPLES; i++) {
+     average += samples[i];
+  }
+  average /= NUMSAMPLES;
+
+  average = 1023 / average - 1;
+  average = SERIESRESISTOR / average;
+
+  float steinhart;
+  steinhart = average / THERMISTORNOMINAL;     // (R/Ro)
+  steinhart = log(steinhart);                  // ln(R/Ro)
+  steinhart /= BCOEFFICIENT;                   // 1/B * ln(R/Ro)
+  steinhart += 1.0 / (TEMPERATURENOMINAL + 273.15); // + (1/To)
+  steinhart = 1.0 / steinhart;                 // Invert
+  steinhart -= 273.15;                         // convert to C
+
+  return steinhart;
 }
 
 void cacheNtcTemperature () {
@@ -55,7 +93,7 @@ void setup() {
     switchOffRelay(i);
   }
   laminator_status = STATUS_NOT_MAPPED;
-  Serial.begin(9600);
+  Serial.begin(115200);
 }
 
 boolean notMapped(int ntc) {
@@ -126,10 +164,10 @@ void validateTemperature() {
 }
 
 void printTemperatures(){  
-  if (print_delay == print_interval) {
+//  if (print_delay == print_interval) {
     int temp;
     for (int i = 0; i < RELAYS_COUNT; i++) {
-      if (laminator_status == STATUS_DEBUG) {
+      if (laminator_status == STATUS_DEBUG || laminator_status == STATUS_MAP_RELAYS) {
         Serial.print(" NTC ");
         temp = getNtcTemperature(i);
       } else {
@@ -143,7 +181,7 @@ void printTemperatures(){
     }
     Serial.println(" ");
     print_delay = 0;
-  }
+//  }
   print_delay++;
 }
 
